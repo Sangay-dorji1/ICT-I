d658af991c3c90aae92efbdf6d4a8d6779460286
Sangay Dorji <08250280.sce@rub.edu.bt>
2026-04-10 15:19:22 +0600

    for loop

---
+++ python demo/forloop.py
@@ -0,0 +1,31 @@
+name = input("Enter your name: ")
+for i in name:
+    print(i)
+
+li = ["python programming", "python fundamentals", "python interview questiion"]
+for x in li:
+    print(x)
+
+lenli = len(li)
+for x in range(lenli):
+    print(li[x])
+
+new_tuple = tuple(li)
+for x in new_tuple:
+    print(x)
+print(type(new_tuple))
+
+tup = ("John Smith", "Jane Doe", "Alice Johnson")
+for x in tup:
+    print(x)
+
+set1 = {10, 30, 20}
+for x in set1:
+    print(x)
+
+
+
+
+
+
+
